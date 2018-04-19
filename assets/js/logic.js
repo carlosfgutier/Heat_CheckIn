5457d3818b524d7f634218eabeeb760cf7176f58
Carlos <carlosfgutier@gmail.com>
2018-04-19 11:18:06 -0500

    fixing duplicate folder (assets/Assets)

---
+++ assets/js/logic.js
@@ -0,0 +1,78 @@
+var campers = [
+	{
+		'id'     : 111111111,
+		'name'   : 'Todd',
+		'last'   : 'Chessher',
+		'status' : 'reserved',
+	},
+	{
+		'id'     : 222222222,
+		'name'   : 'Carlos',
+		'last'   : 'Gutierrez',
+		'status' : 'reserved',
+	},
+	{
+		'id'     : 333333333,
+		'name'   : 'Lee',
+		'last'   : 'Perry',
+		'status' : 'waitlist',
+	},
+	{
+		'id'     : 444444444,
+		'name'   : 'Debbie',
+		'last'   : 'Williams',
+		'status' : 'reserved',
+	},
+	{
+		'id'     : 555555555,
+		'name'   : 'Danny',
+		'last'   : 'Pasa',
+		'status' : 'unregistered',
+	},
+]
+
+$("#check_in_btn").on("click", function() {
+
+	var user_id = $('#id_box').val();
+	var message = $('#message');
+
+	if (user_id.length <=8) {
+		
+		message.html('The ID you entered is not valid. Please try again.');
+
+		setTimeout(clear, 3000);
+	} else {
+
+		for (var i = 0; i < campers.length; i++) {
+
+			var name = campers[i].name;
+			var camper_id = campers[i].id;
+			var status = campers[i].status;
+
+			if (user_id == camper_id && status === 'unregistered') {
+
+				message.html('Hi ' + name + ",<br>You don't seem to be registered.");
+
+				setTimeout(clear, 3000);
+
+			} else if (user_id == camper_id && status === 'waitlist') {
+
+				message.html('Hi ' + name + ",<br>You're still in the waitlist.");
+
+				setTimeout(clear, 3000);
+
+			} else if (user_id == camper_id && status === 'reserved') {
+
+				message.html('Hi ' + name + ",<br>You're good to go!");
+
+				setTimeout(clear, 1500);
+
+			}
+		}
+	}
+});
+
+function clear() {
+	$('#id_box').val('');
+	$('#message').html('');
+}
